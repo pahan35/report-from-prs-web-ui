d9142eb405be7f6055a781a20fbd4a94dd60afe1
Pavlo Zhukov <pahazhukov@gmail.com>
2020-05-29 01:52:28 +0300

    refactor: use snapshot in test names for future convenience

--- src/Components/RenderReport.test.js
+++ src/Components/RenderReport.test.js
@@ -2,7 +2,7 @@ import RequestReport from 'Components/RequestReport'
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-test('RequestReport', () => {
+test('RequestReport snapshot', () => {
   const tree = renderer.create(<RequestReport />).toJSON()
   expect(tree).toMatchSnapshot()
 })
