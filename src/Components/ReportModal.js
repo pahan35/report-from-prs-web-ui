1b9cf5c831b74a119e14c42a83f13ec7f57ca92b
Pavlo Zhukov <pahazhukov@gmail.com>
2020-05-29 04:00:32 +0300

    feat: fake fetch and render reports

--- src/Components/ReportModal.js
+++ src/Components/ReportModal.js
@@ -1,11 +1,36 @@
 import {Dialog, DialogContent, DialogTitle} from 'mdc'
 import React from 'react'
 
-export default function ReportModal({onClose, open}) {
+function ReportSection({label, reports}) {
+  return (
+    <div>
+      <span>{label}</span>
+      {reports
+        ? reports.map(({html_url: htmlUrl, title}) => (
+            <li key={htmlUrl}>
+              {title} [<a href={htmlUrl}>pr</a>]
+            </li>
+          ))
+        : 'No Reports for provided params'}
+    </div>
+  )
+}
+
+export default function ReportModal({onClose, open, reports}) {
+  const sections = [
+    ['done', 'Done'],
+    ['wip', 'WIP'],
+  ]
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Reports</DialogTitle>
-      <DialogContent>TODO pass and render reports</DialogContent>
+      <DialogContent>
+        {reports
+          ? sections.map(([key, label]) => (
+              <ReportSection key={key} label={label} reports={reports[key]} />
+            ))
+          : 'No reports yet'}
+      </DialogContent>
     </Dialog>
   )
 }
--- src/Components/RequestReport.js
+++ src/Components/RequestReport.js
@@ -2,19 +2,42 @@ import ReportModal from 'Components/ReportModal'
 import {Button, Grid, GridCell, TextField} from 'mdc'
 import React, {useState} from 'react'
 
+function fakePRs(repo, from, to) {
+  const prs = []
+  for (let i = from; i < to; i += 1) {
+    prs.push({
+      html_url: `https://github.com/${repo}/pull/${i}`,
+      title: `Fake PR ${i}`,
+    })
+  }
+  return prs
+}
+
+function fetchReports({forDays, repo, user}) {
+  return new Promise(resolve => {
+    console.log('Making requests with', {forDays, repo, user})
+    setTimeout(() => {
+      resolve({
+        done: fakePRs(repo, 1, 5),
+        wip: fakePRs(repo, 8, 10),
+      })
+    }, 350)
+  })
+}
+
 export default function RequestReport() {
   const [forDays, setForDays] = useState(7)
   const [repo, setRepo] = useState()
   const [user, setUser] = useState()
   const [loading, setLoading] = useState(false)
   const [openModal, setOpenModal] = useState(false)
+  const [reports, setReports] = useState()
 
-  const getReports = () => {
+  const getReports = async () => {
     setLoading(true)
-    setTimeout(() => {
-      setOpenModal(true)
-      setLoading(false)
-    }, 3500)
+    setReports(await fetchReports({forDays, repo, user}))
+    setOpenModal(true)
+    setLoading(false)
   }
 
   return (
@@ -50,7 +73,11 @@ export default function RequestReport() {
           Get Reports
         </Button>
       </GridCell>
-      <ReportModal open={openModal} onClose={() => setOpenModal(false)} />
+      <ReportModal
+        open={openModal}
+        reports={reports}
+        onClose={() => setOpenModal(false)}
+      />
     </Grid>
   )
 }
